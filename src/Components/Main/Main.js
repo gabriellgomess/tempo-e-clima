05b81c072459abf66310ea0d80d45dbbef0707fd
gabriellgomess <51470523+gabriellgomess@users.noreply.github.com>
2022-05-06 18:00:14 -0300

    ajuste mobile 3

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -48,7 +48,7 @@ const Main = (props) => {
     }
       
   }, [props.tempo]);
-  console.log(props.tempo.condition_slug)
+  
   return (    
     <div className={`${props.dark===true?"main-dark main col-sm-12 col-md-6 col-lg-6":"main main col-sm-12 col-md-6 col-lg-6"}`}>
         <h1>Tempo Agora</h1>        
